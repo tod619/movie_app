4d9a40d3ee9933ab2749988d4854bf56fb374d8a
tod619 <trevorodwyer619@hotmail.com>
2021-03-17 15:50:29 +0000

    add functionality to search bar in scipt.js file

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ed06ff5dbd4de0485c0eb2df0fcfb35&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARC_URL = 'https://api.themoviedb.org/3/search/movie?api_key=4ed06ff5dbd4de0485c0eb2df0fcfb35&query="'
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=4ed06ff5dbd4de0485c0eb2df0fcfb35&query="'
+
+const form = document.getElementById("form")
+const search = document.getElementById("search")
 
 async function getMovies(url) {
     const res = await fetch(url)
@@ -11,3 +14,18 @@ async function getMovies(url) {
 }
 
 getMovies(API_URL)
+
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== ''){
+        getMovies(SEARCH_URL + searchTerm)
+
+        search.value = ''
+    }else {
+        window.location.reload()
+    }
+})
